c94ac10127048c2d8305c049b31ea8229c6ff843
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-29 08:13:24 +0100

    feat: pass team data into TeamEmblem

--- Features/Fixtures/TeamEmblem.tsx
+++ Features/Fixtures/TeamEmblem.tsx
@@ -1,19 +1,34 @@
 import { View, Image, Text, StyleSheet } from "react-native";
-import * as GlobalConstants from "../../globals/constants";
 
-const TeamEmblem = () => {
+import * as GlobalConstants from "../../globals/constants";
+import { emblems } from "../../globals/images";
+import { FplFixtures } from "../../Models/FplFixtures";
+import { Team } from "../../Models/FplOverview";
+
+interface TeamEmblemProp {
+  team: Team | undefined;
+}
+
+const TeamEmblem = (prop: TeamEmblemProp) => {
   return (
     <View style={styles.teamInfoView}>
-      <Image
-        style={styles.emblems}
-        source={require("../../assets/emblems/eve.png")}
-        resizeMode="contain"
-      />
-      <Text
-        style={{ fontSize: 0.025 * GlobalConstants.width, alignSelf: "center" }}
-      >
-        EVE
-      </Text>
+      {prop.team !== undefined && (
+        <>
+          <Image
+            style={styles.emblems}
+            source={emblems[prop.team.code]}
+            resizeMode="contain"
+          />
+          <Text
+            style={{
+              fontSize: 0.025 * GlobalConstants.width,
+              alignSelf: "center",
+            }}
+          >
+            {prop.team.short_name}
+          </Text>
+        </>
+      )}
     </View>
   );
 };
